d2a7eabb5986ffeb02cb7214a24a9b54a9a86158
anshik <anshiktind@gmail.com>
2024-01-20 19:41:23 +0530

    full navbar working

--- client/lib/axiosConfig.ts
+++ client/lib/axiosConfig.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 const axiosFetch = axios.create({
-  // baseURL: process.env.backendUrl,
+  baseURL: process.env.backendUrl,
   // baseURL: "http://localhost:8080/user",
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
   timeout: 8000,
 });
 
 const axiosFetchAuth = (token: string) =>
   axios.create({
-    baseURL: "http://localhost:8080/auth",
+    baseURL: process.env.backendUrl + "/auth",
     headers: {
       Authorization: `${token}`,
     },
